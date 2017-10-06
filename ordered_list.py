432fbbdce41a34dc3ec97c23b34b4fa4bc94af2e
Tyler <tylrdvs277@gmail.com>
2017-10-06 08:51:44 -0700

    Final Commit for turn in

--- ordered_list.py
+++ ordered_list.py
@@ -30,15 +30,20 @@ class Node:
     def set_prev(self, prev):
         self.prev = prev
 
+
 class OrderedList:
 
     def __init__(self):
+        """Creates a new oredered list using a sentinel node implementation."""
         self.sentinel = Node(None)
         self.sentinel.set_next(self.sentinel)
         self.sentinel.set_prev(self.sentinel)
         self.num_items = 0
 
     def add(self, item):
+        """Adds a new item to the list in numerical order."""
+        """Takes a new number (float or int).
+        Modifies the existing list and returns None."""
         current = self.sentinel.get_next()
         temp = Node(item)
         while current.get_data() != None and current.get_data() < item:
@@ -50,6 +55,9 @@ class OrderedList:
         self.num_items += 1
 
     def remove(self, item):
+        """Removes a specific item in the first place it occurs (from front)."""
+        """Takes an item to be removed.
+        Modifies the existing list and returns None. Raises an error if the item isn't in the list."""
         current = self.sentinel.get_next()
         while current.get_data() != None:
             if current.get_data() == item:
@@ -61,6 +69,9 @@ class OrderedList:
         raise ValueError
 
     def search_forward(self, item):
+        """Checks for an item in the list starting from the front."""
+        """Takes an item to look for.
+        Returns True if the item is in the list and False otherwise."""
         current = self.sentinel.get_next()
         while current.get_data() != None:
             if current.get_data() == item:
@@ -69,6 +80,9 @@ class OrderedList:
         return False
 
     def search_backward(self, item):
+        """Checks for an item in the list starting from the back."""
+        """Takes an item to look for.
+        Returns True if the item is in the list and False otherwise."""
         current = self.sentinel.get_prev()
         while current.get_data() != None:
             if current.get_data() == item:
@@ -77,12 +91,21 @@ class OrderedList:
         return False
 
     def is_empty(self):
+        """Checks if there are any elements in the list."""
+        """Takes no parameters.
+        Returns True if there are no items and False otherwise."""
         return self.num_items == 0
 
     def size(self):
+        """Checks the amount of elements in the list."""
+        """Takes no parameters.
+        Returns an int representing the amount of items in the list."""
         return self.num_items
 
     def index(self, item):
+        """Determines the index of an item in the list."""
+        """Takes an item to look for in the list.
+        Returns an int representing the position of the item in the list."""
         idx = 0 
         current = self.sentinel.get_next()
         while current.get_data() != None:
@@ -93,8 +116,13 @@ class OrderedList:
         raise ValueError
 
     def pop(self, pos = None):
+        """Removes an item at a given position and returns it."""
+        """Takes an int representing an index to remove from.
+        Returns the value at that position and modifies the list."""
         if pos == None:
             pos = self.num_items - 1
+        if pos < 0:
+            pos += self.num_items
         if not (0 <= pos <= self.num_items - 1):
             raise IndexError
         if pos <= self.num_items / 2:
--- ordered_list_tests.py
+++ ordered_list_tests.py
@@ -12,6 +12,7 @@ class TestLab4(unittest.TestCase):
 
 
     def test_add(self):
+        """Tests the add method by adding two items and making sure they are popped correctly."""
         mylist = OrderedList()
         mylist.add(34)
         mylist.add(10)
@@ -21,16 +22,21 @@ class TestLab4(unittest.TestCase):
 
 
     def test_remove(self):
+        """Tests remove by adding two items and removing one.
+        Checks for an error when removing an item not in the list."""
         mylist = OrderedList()
         mylist.add(34)
         mylist.add(10)
         mylist.remove(10)
         self.assertEqual(mylist.size(), 1)
+        self.assertEqual(mylist.pop(0), 34)
         with self.assertRaises(ValueError):
             mylist.remove(35)
 
 
     def test_search_forward(self):
+        """Adds two items and makes sure they are found.
+        Searches for an item not in the list."""
         mylist = OrderedList()
         mylist.add(34)
         mylist.add(10)
@@ -40,6 +46,8 @@ class TestLab4(unittest.TestCase):
 
 
     def test_search_backward(self):
+        """Adds two items and makes sure they are found.
+        Searches for an item not in the list."""
         mylist = OrderedList()
         mylist.add(34)
         mylist.add(10)
@@ -49,6 +57,8 @@ class TestLab4(unittest.TestCase):
 
 
     def test_is_empty(self):
+        """Creates a list, adds an item, then removes.
+        Tests if empty after every operation."""
         mylist = OrderedList()
         self.assertTrue(mylist.is_empty())
         mylist.add(23)
@@ -58,6 +68,8 @@ class TestLab4(unittest.TestCase):
         
 
     def test_size(self):
+        """Creates a list, adds an item, then removes.
+        Checks size after every operation."""
         mylist = OrderedList()
         self.assertEqual(mylist.size(), 0)
         mylist.add(23)
@@ -67,13 +79,17 @@ class TestLab4(unittest.TestCase):
 
 
     def test_pop(self):
+        """Adds ten items to the list.
+        Tests popping at various indices."""
         mylist = OrderedList()
         for i in range(10, 21):
             mylist.add(i)
         self.assertEqual(mylist.pop(0), 10)
         self.assertEqual(mylist.pop(0), 11)
         self.assertEqual(mylist.pop(4), 16)
+        self.assertEqual(mylist.pop(5), 18)
         self.assertEqual(mylist.pop(), 20)
+        self.assertEqual(mylist.pop(-2), 17)
 
 
 if __name__ == "__main__":
